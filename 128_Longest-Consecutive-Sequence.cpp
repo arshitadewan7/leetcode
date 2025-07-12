4186c0ecc3357fa4db29b280e2315094a9c2d235
Arshita Dewan <145092456+arshitadewan@users.noreply.github.com>
2025-07-12 23:56:30 +0930

    second commit

---
+++ 128_Longest-Consecutive-Sequence.cpp
@@ -0,0 +1,11 @@
+// leetcode/128_Longest-Consecutive-Sequence.cpp
+/**
+ * Date  : 12-July-2025
+ * Repo: https://github.com/arshitadewan7/
+ *
+ * Problem   :  Longest Consecutive Sequence
+ * Difficulty:  🟡Medium
+ *
+ * Leetcode 0128 :  https://leetcode.com/problems/longest-consecutive-sequence
+ * Lintcode 0124 :  https://www.lintcode.com/problem/124
+ */
